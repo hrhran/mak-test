17d35e6f7e43edba841980b3f24ca07781c4a1ac
Hariharan <hariharan0044@gmail.com>
2022-02-11 22:02:07 +0530

    yo

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const ejs = require('ejs');
+;
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb+srv://admin:Admin%40123x@testbot.6lv2u.mongodb.net/launchpass?retryWrites=true&w=majority');
+
+const myObject = mongoose.Schema({
+    timestamp: String,
+    createdAt: String,
+    email: String,
+    discord_id: String,
+    code: String,
+    twitter: String,
+    subscription: String,
+  });
+
+  const users = mongoose.model("subscriber", myObject);
+
+app.get('/', (req, res) => {
+    users.find({}, function(err, subscribers) {
+        res.render('index', {
+            subList: subscribers
+        })
+    })
+})
+ e
+app.listen(4000, function() {
+    console.log('server is running');
+})
